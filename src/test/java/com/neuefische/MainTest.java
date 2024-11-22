92b9f15ad18311c2e6c06b8990509219c0e03e19
Coralee Long <coralee.long@gmail.com>
2024-11-22 14:52:41 +0100

    maven intro

---
+++ src/test/java/com/neuefische/MainTest.java
@@ -0,0 +1,17 @@
+package com.neuefische;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void main() {
+        int a = 1;
+        int b = 2;
+        int expected = 2;
+
+        int actual = Main.add(a,b);
+        Assertions.assertEquals(expected,actual);
+    }
+}
